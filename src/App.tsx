310cdf4a2c28be0e5dfdd88de9b0e5669e7ab1d4
Samrouth <bichsamroth@gmail.com>
2024-12-02 15:23:21 +0700

    recact typescript wiht chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card, Flex, Input, Stack } from "@chakra-ui/react";
+import { Field } from "./components/ui/field";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex flex={'flex'} justify={'center'} alignItems={'center'} margin={'auto'} height={'100vb'}>
+      <Card.Root width={'460px'}>
+        <Card.Header>
+          <Card.Title>Sign up</Card.Title>
+          <Card.Description>
+            Fill in the form below to create an account
+          </Card.Description>
+        </Card.Header>
+        <Card.Body>
+          <Stack gap="4" w="full">
+            <Field label="First name">
+              <Input placeholder="First name" />
+            </Field>
+            <Field label="First name">
+              <Input placeholder="Last name" />
+            </Field>
+            <Field label="Email">
+              <Input placeholder="Me@example.com" />
+            </Field>
+            <Field label="Password">
+              <Input placeholder="Password" />
+            </Field>
+            <Field label="Confirm Passowrd">
+              <Input placeholder="Confirm passowrd" />
+            </Field>
+          </Stack>
+        </Card.Body>
+        <Card.Footer justifyContent="flex-end">
+          <Button variant="outline">Cancel</Button>
+          <Button variant="solid">Sign in</Button>
+        </Card.Footer>
+      </Card.Root>
+    </Flex>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createSystem, defaultConfig } from '@chakra-ui/react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      colors: {
+        brand: {
+          900: { value: '#1a365d' },
+          800: { value: '#153e75' },
+          700: { value: '#2a69ac' },
+        },
+      },
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider value={system}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
